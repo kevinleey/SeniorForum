1a6689854feb6ceccbd9b13966ac945395daa8e1
Kevin Lee <nleelar@crimson.ua.edu>
2024-02-05 10:21:42 -0600

    refactor: move navbar components

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/Footer";
 import CategoryBox from "../components/categories/CategoryBox";
 import "../styles/categories.css"
