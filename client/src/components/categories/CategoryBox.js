de95a5e88960c21fba015f8e70e7d8b730e83e2a
CalebSB <csburdette@crimson.ua.edu>
2024-02-05 22:00:40 -0600

    feat: Added Category Box

--- client/src/components/categories/CategoryBox.js
+++ client/src/components/categories/CategoryBox.js
@@ -6,6 +6,7 @@ function CategoryBox(props) {
     return (
         <div id="box-container">
             <div id="content-container">
+                <img src="https://media.istockphoto.com/id/1344512181/vector/icon-red-loudspeaker.jpg?s=612x612&w=0&k=20&c=MSi3Z2La8OYjSY-pr0bB6f33NOuUKAQ_LBUooLhLQsk=" alt="Announcements"/>
                 <div id="category-title">
                     <h3>{props.title}</h3>
                 </div>
