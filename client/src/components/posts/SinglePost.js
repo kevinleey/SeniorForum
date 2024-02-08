82cc4d81e038c53de2a4a49398ad28528a912cc7
Kevin Lee <nleelar@crimson.ua.edu>
2024-02-08 00:15:39 -0600

    feat: further build on SinglePost page and related components

--- client/src/components/posts/SinglePost.js
+++ client/src/components/posts/SinglePost.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Contribution from "./Contribution";
+import { timeSince } from "../../utility/timeSince";
+import "../../styles/single-post.css";
 
-function SinglePost({ post }) {
-  return <div>hi this is post {post._id}</div>;
+function SinglePost({ post, comments }) {
+  return (
+    <div id="single-post-container">
+      <div id="single-post-header">
+        <h1 className="page-title">{post.title}</h1>
+        <h3>
+          {timeSince(post.dateCreated)} by {post.createdBy.firstName}{" "}
+          {post.createdBy.lastName}
+        </h3>
+      </div>
+      <Contribution contribution={post} />
+      <div id="single-post-comments-container">
+        <h2>Comments {}</h2>
+        {comments.map((comment) => (
+          <Contribution contribution={comment} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SinglePost;
--- client/src/pages/SinglePostPage.js
+++ client/src/pages/SinglePostPage.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/navbar/Navbar";
 import SinglePost from "../components/posts/SinglePost";
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectAllPosts } from "../features/posts/postsSlice";
+import {
+  selectCommentsByPostId,
+} from "../features/comments/commentsSlice";
 import Error from "../components/Error";
 import { ERROR_MESSAGES as error } from "../constants";
+import fetchCommentsForPost from "../features/comments/commentsThunks";
 
 function SinglePostPage() {
+  const dispatch = useDispatch();
   const { postId } = useParams();
   const posts = useSelector(selectAllPosts);
+  const comments = useSelector(selectCommentsByPostId);
+
+  useEffect(() => {
+    dispatch(fetchCommentsForPost(postId));
+  }, [dispatch, postId]);
 
   const post = posts.filter((post) => post._id === postId);
 
   return (
     <div>
       <Navbar />
-      <div className="container">
+      <div className="page-container">
         {post.length ? (
-          <SinglePost post={post[0]} />
+          <SinglePost post={post[0]} comments={comments} />
         ) : (
           <Error message={error.INVALID_POSTID} />
         )}
