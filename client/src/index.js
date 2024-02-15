feb0095a8d205d595ed5a4e975834f7c85c37b35
Jacob Aid <jacobaid103@yahoo.com>
2024-02-14 18:37:43 -0600

    feat: added user data to redux store. Displaying data on home page.

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,19 @@ import App from "./App.js";
 import "./styles/global.css";
 import store from "./app/store";
 import { Provider } from "react-redux";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <App />
     </Provider>
   </React.StrictMode>,
 );
+
+/*       <Auth0Provider
+            domain={process.env.AUTH0_DOMAIN}
+            clientId={process.env.CLIENT_ID}
+            redirectUri={window.location.origin}>
+        </Auth0Provider>    */
