9b05522f2188352a245fc374362990c67d4133b9
Kevin Lee <nleelar@crimson.ua.edu>
2024-02-15 18:18:56 -0600

    feat: add some basic support for displaying username in home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,10 +3,10 @@ import Navbar from "../components/navbar/Navbar";
 import { useSelector, useDispatch } from "react-redux";
 import { selectAllPosts } from "../features/posts/postsSlice";
 import PostList from "../components/posts/PostList";
-import UserList from "../components/users/userList";
 import Footer from "../components/Footer";
 import {selectAllUsers, selectCurrentUser, setCurrentUser} from "../features/users/userSlice";
 import { fetchCurrUser } from "../features/users/userThunks";
+import { HOME_RESOURCES } from "../constants";
 import "../styles/home.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -25,20 +25,13 @@ function Home() {
   }, [dispatch, isLoading, auth0User]);
 
   return (
-      <div id="page-overview">
-          <Navbar/>
-          <div className="page-container">
-              <div id="posts-box">
-                  <h1 className="page-title">
-                      {currentUser ? `Welcome ${currentUser.firstName}, here are some recent posts.` : "Welcome, here are some recent posts. Login to join the community!"}
-                  </h1>
-                  <PostList posts={posts}/>
-              </div>
-          </div>
-
-      <div className="user-list-container">
-        <h2>Current Users:</h2>
-        <UserList users={users} />
+    <div id="page-background">
+      <Navbar />
+      <div className="page-container">
+        <h1 className="page-title">
+          {currentUser ? `Welcome ${currentUser.firstName}, here are some recent posts.` : "Welcome, here are some recent posts. Login to join the community!"}
+        </h1>
+        <PostList posts={posts} />
       </div>
       <Footer />
     </div>
