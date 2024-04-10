a96566418859350ed7011af43dff7d3c5630e0de
CalebSB <csburdette@crimson.ua.edu>
2024-04-10 14:57:52 -0500

    feat: adding sorting and searching functionality with posts.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar/Navbar";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { selectAllPosts } from "../features/posts/postsSlice";
 import PostList from "../components/posts/PostList";
 import Footer from "../components/Footer";
-import { selectCurrentUser } from "../features/users/userSlice";
+import { selectCurrentUser, setCurrentUser } from "../features/users/userSlice";
+import { fetchCurrUser } from "../features/users/userThunks";
 import "../styles/home.css";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
@@ -12,7 +13,11 @@ import Spinner from "../components/Spinner";
 
 function Home() {
   const posts = useSelector(selectAllPosts);
+  const [sortedPosts, setSortedPosts] = useState(posts);
   const currentUser = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
+  const selectSort = document.getElementById("sorting-dropdown");
+  // const { user: auth0User, isLoading } = useAuth0();
   const navigate = useNavigate();
   const { isLoading } = useAuth0();
   const error = useSelector((state) => state.users.error);
@@ -27,20 +32,38 @@ function Home() {
     return <Spinner />;
   }
 
+  const sortPosts = (e) => {
+    // const value = e.target.value;
+    // console.log(value);
+    // let sortedPosts;
+    // if (value === "popular") {
+    //   sortedPosts = [...posts].sort((a, b) => b.comments.length - a.comments.length);
+    // } else {
+    //   sortedPosts = [...posts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    // }
+    // dispatch()
+  };
+
   return (
     <div id="page-background">
       <Navbar />
       <div id="page-container">
-        <h1 className="page-title">
-          {/*isAuthenticated ? 'You are authenticated! ' : 'You are not authenticated! '*/}
-          {currentUser
-            ? `Welcome ${currentUser.firstName}, here are some recent posts.`
-            : "Welcome, here are some recent posts. Login to join the community!"}
-        </h1>
-        {error && <div>Error: {error}</div>}
-        <PostList posts={posts} />
+        <div id="page-title">
+          <h1 className="page-header">
+            {/*isAuthenticated ? 'You are authenticated! ' : 'You are not authenticated! '*/}
+            {currentUser
+              ? `Welcome ${currentUser.firstName}, here are some recent posts.`
+              : "Welcome, here are some recent posts. Login to join the community!"}
+          </h1>
+          {error && <div>Error: {error}</div>}
+          <select id="sorting-dropdown" onChange={(e) => sortPosts(e)}>
+            <option value="recent">Recent</option>
+            <option value="popular">Popular</option>
+          </select>
+        </div>
+        <PostList posts={posts}/>
       </div>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
